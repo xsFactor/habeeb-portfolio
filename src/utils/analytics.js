00bdfea9b8ad57d481558359364657b3386898f7
BrainARD-hub <ahmedbrainard101@gmail.com>
2026-05-01 01:52:06 +0100

    Add Google Analytics tracking

--- src/utils/analytics.js
+++
@@ -1,8 +0,0 @@
-export const trackEvent = (action, category = "Portfolio", label = "") => {
-  if (window.gtag) {
-    window.gtag("event", action, {
-      event_category: category,
-      event_label: label,
-    });
-  }
-};
