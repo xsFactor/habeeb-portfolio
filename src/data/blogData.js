c67eb72f4564a906a05d911ff01c1aa69ac8ed95
BrainARD-hub <ahmedbrainard101@gmail.com>
2026-05-10 10:10:12 +0100

    Add Google Analytics GA4 tracking

--- src/data/blogData.js
+++ src/data/blogData.js
@@ -1,25 +1,25 @@
 export const blogData = [
     {
         id: 1,
-        title: 'SCRAPING BILLBOARD AND IMDB USING PYTHON SELENIUM',
-        description: 'Here we will use Selenium to navigate between web pages, and try to scrape data from them.',
-        date: 'Aug 7, 2020',
-        image: 'https://1.bp.blogspot.com/-6vZQoWcIZeo/Xvw5ZD5Y53I/AAAAAAAABgI/GH2b5OuMaXAKuigK4diBbeYDW6dD3qVvgCK4BGAsYHg/w400-h225/selenium.jpg',
-        url: 'https://hackzism.blogspot.com/2020/07/scraping-billboard-and-imdb-using.html'
+        title: 'PHISHING SIMULATION LAB DEMO WITH OTP BYPASS',
+        description: 'In this guide, you’ll learn how to build a Security Awareness Training Lab on Windows. This lab simulates phishing scenarios in a safe and controlled environment.',
+        date: 'May 7, 2026',
+        image: 'https://miro.medium.com/v2/resize:fit:720/format:webp/1*0ctgcN6PhqANM4macHQT_Q.png',
+        url: 'https://medium.com/@CivicMilCyber/%EF%B8%8F-how-to-set-up-a-security-awareness-training-lab-on-windows-beginner-friendly-294000248111'
     },
     {
         id: 2,
-        title: 'CHECK WEATHER FROM TERMINAL USING WTTR.IN',
-        description: 'wttr.in is a console-oriented weather forecast service that supports various information representation methods like ANSI-sequences for console HTTP clients, HTML, or PNG.',
-        date: 'Aug 11, 2020',
-        image: 'https://1.bp.blogspot.com/-OW7jX57tea4/XvnGxuEOslI/AAAAAAAABW0/R8lVT1AXDSwnvE0EGA9Ra49-LDm1ACwDgCK4BGAsYHg/s1216/wttr1.png',
-        url: 'https://hackzism.blogspot.com/2020/06/check-weather-from-terminal-using-wttrin.html'
+        title: 'UNDERSTANDING THE THREAT IN THE DIGITAL AGE',
+        description: 'This article examines the foundations of disinformation analysis, including its definition, various forms, and the reasons behind its widespread online dissemination.',
+        date: 'Aug 31, 2025',
+        image: 'https://miro.medium.com/v2/resize:fit:720/format:webp/1*TNBfWYr3eocs8Y1x5uNGTA.png',
+        url: 'https://medium.com/@CivicMilCyber/foundations-of-disinformation-analysis-understanding-the-threat-in-the-digital-age-d9ac499e501c'
     },
     {
         id: 3,
         title: 'BEING ANONYMOUS: A BEGINNERS GUIDE',
         description: 'Here are some basic ways to lay off the Radar',
-        date: 'Aug 14, 2020',
+        date: 'Aug 31, 2025',
         image: 'https://1.bp.blogspot.com/-GR19yCNbKCE/XuRcKEfKvhI/AAAAAAAAAwY/srFWw5pOpzMibeJb__SmgZpx9dIpNNWqgCK4BGAsYHg/w640-h426/anonymous1.jpg',
         url: 'https://hackzism.blogspot.com/2020/06/being-anonymous-beginners-guide.html'
     },
